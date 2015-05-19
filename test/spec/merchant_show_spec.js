d36fca8bf429498f6b1d6f69b3b3365ad63e85f6
Prashant Sahni <prashant.sahni5@gmail.com>
2015-05-19 16:19:17 +0530

    commented correct test case uncommented

--- test/spec/merchant_show_spec.js
+++ test/spec/merchant_show_spec.js
@@ -10,44 +10,44 @@ describe("Merchants", function(){
         ppPage.getRoot();
     });
 
-//    it("should load merchant show page", function(){
-//
-//        ppPage.login();
-//
-//        // Via Pipeline
-//        var pipeline_link = element(by.css("a[href*=pipeline]"));
-//        pipeline_link.click().then(function(){
-//            var submission_link = element.all(by.css("a[href*=options]"));
-//            submission_link.then(function(ele){
-//                expect(ele[0].getText()).toBe('OPTIONS');
-//
-//                ele[0].click().then(function(){
-//                    browser.sleep(5000);
-//                    element.all(by.css(' #options-items .item-title ')).then(function(el){
-//                        el[0].click();
-//                        browser.sleep(5000);
-//                        var collapser = element(by.css('.collapseInit'));
-//                        expect(collapser.getText()).toBe('Options Available');
-//
-//                        var op = element(by.css('.merchant-app-options'));
-//                        expect(op.isPresent()).toBeTruthy();
-//                    });
-//                });
-//            });
-//        });
-//
-//        // Show Page
-//        browser.get("/merchant/227004/show").then(function(){
-//            expect(browser.getTitle()).toMatch(/Merchant/);
-//            browser.getCurrentUrl().then(function(url){
-//                expect(url).toMatch(/merchant/);
-//                expect(url).toMatch(/show/);
-//
-//                var owner = element(by.css('.member-details'));
-//                expect(owner.isPresent()).toBeTruthy();
-//            });
-//        });
-//    });
+    it("should load merchant show page", function(){
+
+        ppPage.login();
+
+        // Via Pipeline
+        var pipeline_link = element(by.css("a[href*=pipeline]"));
+        pipeline_link.click().then(function(){
+            var submission_link = element.all(by.css("a[href*=options]"));
+            submission_link.then(function(ele){
+                expect(ele[0].getText()).toBe('OPTIONS');
+
+                ele[0].click().then(function(){
+                    browser.sleep(5000);
+                    element.all(by.css(' #options-items .item-title ')).then(function(el){
+                        el[0].click();
+                        browser.sleep(5000);
+                        var collapser = element(by.css('.collapseInit'));
+                        expect(collapser.getText()).toBe('Options Available');
+
+                        var op = element(by.css('.merchant-app-options'));
+                        expect(op.isPresent()).toBeTruthy();
+                    });
+                });
+            });
+        });
+
+        // Show Page
+        browser.get("/merchant/227004/show").then(function(){
+            expect(browser.getTitle()).toMatch(/Merchant/);
+            browser.getCurrentUrl().then(function(url){
+                expect(url).toMatch(/merchant/);
+                expect(url).toMatch(/show/);
+
+                var owner = element(by.css('.member-details'));
+                expect(owner.isPresent()).toBeTruthy();
+            });
+        });
+    });
 
     iit("should able user to go from pipeline to loan calculator", function(){
         ppPage.login();
