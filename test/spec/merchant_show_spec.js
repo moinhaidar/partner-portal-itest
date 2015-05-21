a5ed85c0304ec35eb04d19b37e3a0abbf41a7ce2
Prashant Sahni <prashant.sahni5@gmail.com>
2015-05-21 19:17:56 +0530

    testcase for mca option

--- test/spec/merchant_show_spec.js
+++ test/spec/merchant_show_spec.js
@@ -49,7 +49,7 @@ describe("Merchants", function(){
         });
     });
 
-    iit("should able user to go from pipeline to loan calculator", function(){
+    it("should able user to go from pipeline to loan calculator", function(){
         ppPage.login();
         var pipeline_link = element(by.css("a[href*=pipeline]"));
         pipeline_link.click().then(function(){
@@ -77,5 +77,31 @@ describe("Merchants", function(){
             });
         });
     });
+
+//    it("should check for merchant cash advance(MCA)", function(){
+//        ppPage.login();
+//        var pipeline_link = element(by.css("a[href*=pipeline]"));
+//        pipeline_link.click().then(function(){
+//            var options_link = element.all(by.css("a[href*=options]"));
+//            options_link.then(function(ele){
+//                ele[0].click().then(function(){
+//                    browser.sleep(5000);
+//                    element.all(by.css('#options-items .collapseInit')).then(function(el){
+//                        el[1].click();
+//                        browser.sleep(5000);
+//                        element.all(by.css('#options1 .offer-box a')).then(function(eleme){
+//                            eleme[0].click();
+//                            expect(eleme[0].getText()).toBe('MCA')
+//                            var buttons = element.all(by.css('.fancybox-wrap .loan-buttons .close-fancybox'));
+//                            buttons.then(function(button){
+//                                button[0].click();
+//                            });
+//                        });
+//                    });
+//                });
+//            });
+//
+//        });
+//    });
 
 });
